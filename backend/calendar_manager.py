76764f5de3d14fc737608eff2071405d6609be70
BlackTears12 <szombati.balint@gmail.com>
2025-06-11 16:01:20 +0200

    Moved source files to src

--- backend/calendar_manager.py
+++
@@ -1,8 +0,0 @@
-from gcsa.google_calendar import GoogleCalendar
-
-
-class CalendarManager:
-    def __init__(self,email_address: str):
-        self.calendar = GoogleCalendar(email_address)
-        for event in self.calendar:
-            print(event)
